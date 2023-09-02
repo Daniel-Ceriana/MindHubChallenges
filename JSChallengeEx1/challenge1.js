6dbe4b226974561d37d2104920a6b41ab32b557a
Daniel Ceriana <danielcerianax@gmail.com>
2023-09-02 14:56:24 -0300

    [FEAT] Funciona la comparacion, pero no cambia los nombres de los episodios

--- JSChallengeEx1/challenge1.js
+++ JSChallengeEx1/challenge1.js
@@ -9,140 +9,59 @@ async function getData() {
 async function initApp() {
     await getData()
 
-    // console.log(data)
+    console.log(await compareCharacter(data))
 
-    compareCharacter(data)
-        // sharedEpisodes(data)
+    // sharedEpisodes(data)
 }
 
 initApp()
 
-
-//el primero en el array es al que se van a comparar (recibe ids)
 async function compareCharacter(characterArray) {
+    const normalizedArray = [];
+    //"mainCharacter" = el personaje al que se van a comparar los "secondaryCharacter"
+    await characterArray[0].results.forEach(async mainCharacter => {
+        const comparedArray = [];
 
-    const aux = []
-    const comparedArray = [];
-    await characterArray[0].results.forEach(mainCharacter => {
-
-
-        characterArray[0].results.forEach(secondaryCharacter => {
-            console.log("Comparando " + mainCharacter.name + " con " + secondaryCharacter.name)
+        await characterArray[0].results.forEach(secondaryCharacter => {
 
             if (mainCharacter.name != secondaryCharacter.name) {
+                const aux = []
+
                 mainCharacter.episode.forEach(episode => {
-                    comparedArray.push({
-                        character: mainCharacter.name,
-                        otherCharacter: secondaryCharacter.name,
-                        episodes: secondaryCharacter.episode.filter(secondayCharacterEpisode => secondayCharacterEpisode == episode).flat()
-                    })
-
-
-
+                    aux.push(secondaryCharacter.episode.filter(secondayCharacterEpisode => secondayCharacterEpisode == episode))
                 });
+
+                try {
+                    comparedArray.push({
+                        otherCharacterName: secondaryCharacter.name,
+                        // -------------------------- Cambio de nombre no funciona-------------------------------
+                        // al final de todo quedan como promesas, se ejecuta en desorden
+                        // episodesShared: aux.flat().map(async urlEpisode => { urlEpisode = await getEpisodeName(urlEpisode) })
+                        episodesShared: aux.flat()
+                    })
+                } catch (error) {
+                    console.log(error)
+                }
             }
-            // console.log(comparedArray)
-            console.log(comparedArray)
-            aux.push({
-                mainName: mainCharacter.name,
-                secondaryName: secondaryCharacter.name,
-                episodes: comparedArray
-            });
-            // console.log(character)
-            // console.log(aux)
-            comparedArray.map(element => comparedArray.pop());
+
         });
-
-
-
-
-
+        normalizedArray.push({
+            character: mainCharacter.name,
+            otherCharacters: comparedArray
+        })
     });
-
+    return normalizedArray
 }
-
-function asd(auxiliar) {
-    const comparedArray = []
-
-    auxiliar.forEach(character => {
-        // console.log(character)
-    });
-
-}
-
 
 async function getEpisodeName(urlEpisode) {
-    let res;
-    await fetch(urlEpisode)
-        .then(response => response.json())
-        .then(json => res = json.name);
-    console.log(res);
-    return res;
-}
-
-// function sharedEpisodes()
-
-const DisplayController = () => {
-    const display = document.querySelector("main");
-
-    function displayMainCharacter(character) {
-        // display.textContent = "";
-        const article = document.createElement('article');
-        article.setAttribute('class', 'character');
-        article.innerHTML = `
-        <section class="card">
-        <h2>${character.name}</h2>
-        <img src="${character.image}", alt="Character ${character.name} from Rick and Morty">
-        </section>
-        `
-        display.appendChild(article);
+    if (typeof urlEpisode != undefined && typeof urlEpisode != null) {
+        try {
+            const res = await fetch(urlEpisode);
+            const data = await res.json();
+            console.log(data.name)
+            return data.name;
+        } catch (error) {
+            console.log(error);
+        }
     }
-
-    function displaySecondaryCharacter(character, numberOfEpisodes) {
-        // display.textContent = "";
-        const article = document.createElement('article');
-        article.setAttribute('class', 'character');
-        article.innerHTML = `
-        <section class="card--character__compare">
-        <article>
-        <h2>${character.name}</h2>
-        <img src="${character.image}", alt="Character ${character.name} from Rick and Morty">
-        <p>Episodes together: ${numberOfEpisodes}</p>
-        </article>
-
-        <article class="display--episodes" id="display-${character.id}">
-
-
-        </article>
-        </section>
-
-
-
-    `
-        display.appendChild(article);
-    }
-
-    function displayEpisodesTogether(id, episodeName) {
-        const displayEpisodes = document.querySelector(`#display-${id}`)
-            // display.textContent = "";
-        const article = document.createElement('article');
-
-
-        article.setAttribute('class', 'character');
-        article.innerHTML = `
-        <section class="episode-name-minicard">
-        <p>${episodeName}</p>
-        </section>
-
-
-
-    `
-        displayEpisodes.appendChild(article);
-
-    }
-
-
-
-
-    return { displayMainCharacter, displaySecondaryCharacter, displayEpisodesTogether }
 }
