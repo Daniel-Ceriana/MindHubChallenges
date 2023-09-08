0f294d0c580f3b12b26622ed1c25e9b84c4c1554
Daniel Ceriana <danielcerianax@gmail.com>
2023-09-07 21:21:18 -0300

    terminado ejercicio 7

--- Aplicando logica 2/JS 7/scripts/script.js
+++ Aplicando logica 2/JS 7/scripts/script.js
@@ -1,36 +1,48 @@
-const datos = ["Afganistán", "Albania", "Alemania", "Andorra", "Angola", "Antigua y Barbuda",
+const paises = ["Afganistán", "Albania", "Alemania", "Andorra", "Angola", "Antigua y Barbuda",
     "Arabia Saudita", "Argelia", "Argentina", "Armenia"
 ]
+const numeros = [1, 2, 3, 4, 5, 6];
 
-const datosDisplay = document.getElementById("array");
+const paisesDisplay = document.getElementById("paises");
+const numerosDisplay = document.getElementById("numeros");
 const buttons = document.querySelectorAll("button")
 const buttonsLength = buttons.length;
 for (let i = 0; i < buttonsLength; i++) {
     buttons[i].addEventListener("click", (e) => {
+        paisesDisplay.innerHTML = ""
         switch (e.target.id) {
-            case "map":
-
-                break;
             case "for-each":
-                datos.forEach(dato => { datosDisplay.innerHTML += `${dato}, ` })
+                paises.forEach(dato => { paisesDisplay.innerHTML += `${dato}, ` })
+                break;
+            case "map":
+                paisesDisplay.innerHTML = paises.map(dato => dato = dato.toUpperCase())
                 break;
             case "filter":
-
+                paisesDisplay.innerHTML = paises.filter(dato => dato.length < 9)
                 break;
             case "reduce":
-
+                numerosDisplay.innerHTML = numeros.reduce((datoPrevio, dato) => datoPrevio * dato);
                 break;
             case "find":
-
+                numerosDisplay.innerHTML = numeros.find(dato => dato > 5)
                 break;
             case "find-index":
-
+                const index = paises.findIndex(dato => dato.length > 13)
+                paisesDisplay.innerHTML = `${paises[index]}: ${index} `
                 break;
             case "some":
-
+                paisesDisplay.innerHTML = paises.some(dato => dato === "Argentina")
                 break;
             case "sort":
-
+                paisesDisplay.innerHTML = paises.sort((datoA, datoB) => {
+                    if (datoA > datoB) {
+                        return -1;
+                    }
+                    if (datoA < datoB) {
+                        return 1;
+                    }
+                    return 0
+                })
                 break;
 
             default:
